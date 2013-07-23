e265588ca880f0e750c6351169e8a1ce59fc3a00
shaolang <shaolang@ymail.com>
2013-07-23 22:36:29 +0800

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :profiles {:dev {:dependencies [[bultitude "0.2.0"]
-                                  [lazytest "1.2.3"]
-                                  [midje "1.4.0"]
-                                  [org.clojure/clojure "1.4.0"]
+                                  [midje "1.5.1"]
+                                  [org.clojure/clojure "1.5.1"]
                                   [org.mockito/mockito-all "1.9.5"]]
-                   :plugins [[lein-midje "2.0.4"]]}
+                   :plugins [[lein-midje "3.0.1"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC1"]]}}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}}
   :aliases {"all" ["with-profile"
-                   "dev:dev,1.3:dev,1.5"]})
+                   "dev:dev,1.3:dev,1.4:dev,1.5"]})
